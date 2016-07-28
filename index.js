48e0e0049a83142e2529321d925a00adf0d55a79
Necktrox <necktrox@gmail.com>
2016-07-28 05:48:47 +0200

    Add package.json & LICENSE

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // all you need to do is require this ("var x = require('discord-emoji')") and then you have x.food.pizza
 
 module.exports = {
-  activity = require('./activity.json'),
-  emoji = require('./emoji.json'),
-  flags = require('./flags.json'),
-  food = require('./food.json'),
-  nature = require('./nature.json'),
-  objects = require('./objects.json'),
-  people = require('./people.json'),
-  symbols = require('./symbols.json'),
-  travel = require('./travel.json')
+    activity = require('./activity.json'),
+    emoji = require('./emoji.json'),
+    flags = require('./flags.json'),
+    food = require('./food.json'),
+    nature = require('./nature.json'),
+    objects = require('./objects.json'),
+    people = require('./people.json'),
+    symbols = require('./symbols.json'),
+    travel = require('./travel.json')
 }
