3f11f55357a0967b2c97731210aeb77219394e2d
Gus Caplan <me@guscaplan.me>
2016-07-27 22:36:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // all you need to do is require this ("var x = require('discord-emoji')") and then you have x.food.pizza
 
 module.exports = {
-  activity = require('./activity.js'),
-  emoji = require('./emoji.js'),
-  flags = require('./flags.js'),
-  food = require('./food.js'),
-  nature = require('./nature.js'),
-  objects = require('./objects.js'),
-  people = require('./people.js'),
-  symbols = require('./symbols.js'),
-  travel = require('./travel.js')
+  activity = require('./activity.json'),
+  emoji = require('./emoji.json'),
+  flags = require('./flags.json'),
+  food = require('./food.json'),
+  nature = require('./nature.json'),
+  objects = require('./objects.json'),
+  people = require('./people.json'),
+  symbols = require('./symbols.json'),
+  travel = require('./travel.json')
 }
