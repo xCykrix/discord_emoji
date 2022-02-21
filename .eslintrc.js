4e48cfdcd8c0aba7cc5702513ea1a01399745ac8
Samuel J Voeller <12182390+xCykrix@users.noreply.github.com>
2022-02-21 04:01:47 +0100

    feat: sync working changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,20 @@
-.rc/script/configuration/.trunk/_/.eslintrc.js
+const { resolve } = require("path");
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: resolve(__dirname, '../../../../../'), // Because ESLint follows the symbolic link...
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["import"],
+  extends: [
+    "eslint:recommended",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+  rules: {
+    '@typescript-eslint/explicit-member-accessibility': 'error'
+  },
+};
