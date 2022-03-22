aee5337633e8630cb3e451a1707308e74ac6d06f
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2022-03-22 15:25:33 -0400

    test: add mod.test.ts suite

--- mod.test.ts
+++ mod.test.ts
@@ -1,6 +1,42 @@
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+import * as dismoji from './mod.ts';
 
 // Define the QA UnitTest for 'people' in the runtime.
 Deno.test('people', () => {
-  assertEquals(true, true);
+  assertEquals(dismoji.people.grinning, '😀');
+});
+
+// Define the QA UnitTest for 'nature' in the runtime.
+Deno.test('nature', () => {
+  assertEquals(dismoji.nature.dog, '🐶');
+});
+
+// Define the QA UnitTest for 'food' in the runtime.
+Deno.test('food', () => {
+  assertEquals(dismoji.food.hamburger, '🍔');
+});
+
+// Define the QA UnitTest for 'activity' in the runtime.
+Deno.test('activity', () => {
+  assertEquals(dismoji.activity.basketball, '🏀');
+});
+
+// Define the QA UnitTest for 'travel' in the runtime.
+Deno.test('travel', () => {
+  assertEquals(dismoji.travel.airplane, '✈️');
+});
+
+// Define the QA UnitTest for 'objects' in the runtime.
+Deno.test('objects', () => {
+  assertEquals(dismoji.objects.watch, '⌚');
+});
+
+// Define the QA UnitTest for 'symbols' in the runtime.
+Deno.test('symbols', () => {
+  assertEquals(dismoji.symbols.eight_pointed_black_star, '✴️');
+});
+
+// Define the QA UnitTest for 'flags' in the runtime.
+Deno.test('flags', () => {
+  assertEquals(dismoji.flags.flag_us, '🇺🇸');
 });
