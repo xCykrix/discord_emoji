41fc4315d938c31a46135120773d540bbc1087c5
Samuel Voeller <samuel.voeller@amethyst.live>
2022-09-08 19:07:52 -0400

    feat: update to latest supported twemoji

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 // Export variables related to the application in multiple locations.
 export const name = 'discord-emoji';
 export const description = 'A near exact emoji tables of Discord for string-based insertion of emotes without having to escape Unicode.';
-export const version = '2.0.6';
+export const version = '2.1.0';
 
 /** assert */
 export { assert, assertEquals } from 'https://deno.land/std@0.152.0/testing/asserts.ts';
