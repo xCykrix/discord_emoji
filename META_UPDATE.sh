431ab8b31f71689effdedd8b6002230a3ec4df08
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2023-05-16 16:37:01 -0400

    chore(meta): update cross-organization state

--- META_UPDATE.sh
+++ META_UPDATE.sh
@@ -9,6 +9,7 @@ git clone https://github.com/amethyst-studio/meta "${TEMP_DIR}"
 echo "Installing githooked if needed."
 bash META_INIT.sh
 
+# Update Base Files
 echo "Pulling the files which will be updated."
 cp -r "./${TEMP_DIR}/META_INIT.sh" "./META_INIT.sh"
 cp -r "./${TEMP_DIR}/META_UPDATE.sh" "./META_UPDATE.sh"
@@ -16,6 +17,7 @@ cp -r "./${TEMP_DIR}/.editorconfig" "./.editorconfig"
 cp -r "./${TEMP_DIR}/.gitattributes" "./.gitattributes"
 cp -r "./${TEMP_DIR}/.git-hooks/prepare-commit-msg" "./.git-hooks/prepare-commit-msg"
 
+# Update GitHub
 if ! [[ "$PWD" =~ ".github" ]]; then
   cp -r "./${TEMP_DIR}/.github" "./"
   if ! [[ "$PWD" =~ "meta" ]]; then
@@ -26,6 +28,13 @@ if ! [[ "$PWD" =~ ".github" ]]; then
   fi
 fi
 
+# Update Deno-Specific if deno.jsonc is detected.
+if [[ -f "./deno.jsonc" ]]; then
+  mkdir -f "./.vscode"
+  cp -r "./${TEMP_DIR}/.vscode/settings.json" "./.vscode/"
+  cp -r "./${TEMP_DIR}/deno.jsonc" "./"
+fi
+
 # Remove the UPDATE_STATE_LOCK_X1 dir.
 rm -rf "./${TEMP_DIR}/"
 rm -rf githooked_linux
@@ -33,12 +42,14 @@ rm -rf githooked_linux
 # Commit the changes.
 echo "Commiting files to the selected branch."
 git reset
-git add META_INIT.sh
-git add META_UPDATE.sh
+git add ./.git-hooks/prepare-commit-msg
 git add .github
+git add .vscode
 git add .editorconfig
 git add .gitattributes
-git add ./.git-hooks/prepare-commit-msg
+git add deno.jsonc
+git add META_INIT.sh
+git add META_UPDATE.sh
 git commit -m "chore(meta): update cross-organization state"
 
 # Notice.
