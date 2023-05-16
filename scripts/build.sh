4d23e5995805ed78f1efb8848b4f208bb8b3f776
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2023-05-16 16:41:44 -0400

    feat: update local scripts

---
+++ scripts/build.sh
@@ -0,0 +1,2 @@
+deno cache --no-check=remote ./scripts/build-mod.ts
+deno run -A ./scripts/build-mod.ts
---
+++ scripts/dnt.sh
@@ -0,0 +1,2 @@
+deno cache --no-check=remote --reload ./scripts/build-npm.ts
+deno run -A ./scripts/build-npm.ts
