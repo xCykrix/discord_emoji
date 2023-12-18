0de3c1b8fd5c9655f908428f2847d0bb6941bfa8
Samuel <12182390+xCykrix@users.noreply.github.com>
2023-12-18 15:27:28 -0500

    feat: update working tree

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 // Export variables related to the application in multiple locations.
 export const name = 'discord-emoji';
 export const description = 'A near exact emoji tables of Discord for string-based insertion of emotes without having to escape Unicode.';
-export const version = '2.3.0';
+export const version = '2.4.0';
 
 /** assert */
 export { assert, assertEquals } from 'https://deno.land/std@0.209.0/testing/asserts.ts';
