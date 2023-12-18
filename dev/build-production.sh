e2a714b272d31e8d3c5b1ed9a5f26ee8aa13e353
Samuel <12182390+xCykrix@users.noreply.github.com>
2023-12-18 14:17:30 -0500

    fix: allow writing to deno cache for npm build task

--- dev/build-production.sh
+++ dev/build-production.sh
@@ -12,7 +12,7 @@ WASMBUILD=$HOME/.local/share/deno-wasmbuild
 NPM=$(which npm)
 
 # Run Build (with Security Scope)
-deno run --no-prompt --allow-net=discord.com --allow-read=$DENO_CACHE,$DENO_EXEC,./ --allow-write=mod.ts,js_temp --allow-run=$DENO_EXEC ./dev/build.ts
+deno run --no-prompt --allow-net=discord.com --allow-read=$DENO_CACHE,$DENO_EXEC,./ --allow-write=$DENO_CACHE,mod.ts,js_temp --allow-run=$DENO_EXEC ./dev/build.ts
 
 # Build Production
-deno run --no-prompt --allow-read=$DENO_CACHE,$WASMBUILD,dist,./ --allow-write=$WASMBUILD,dist --allow-run=$NPM --allow-env=HOME,DENO_DIR,DENO_AUTH_TOKENS,XDG_DATA_HOME,XDG_CACHE_HOME --allow-net ./dev/dist.ts
+deno run --no-prompt --allow-read=$DENO_CACHE,$WASMBUILD,dist,./ --allow-write=$DENO_CACHE,$WASMBUILD,dist --allow-run=$NPM --allow-env=HOME,DENO_DIR,DENO_AUTH_TOKENS,XDG_DATA_HOME,XDG_CACHE_HOME --allow-net ./dev/dist.ts
