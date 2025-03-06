6c806c8b6393db0e7bf9c6541c4b25f148069a3d
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2025-03-06 04:06:44 +0000

    refactor(build): additional enhancements on build rewrite

--- dev/setup.sh
+++ dev/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 # DevOps: githooked - https://xcykrix.github.io/githooked.html#githooked-installation-and-help
 curl -s https://api.github.com/repos/xCykrix/githooked/releases/latest |
-  grep "browser_download_url.*" |
+  grep "browser_download_url.*/githooked/.*/githooked" |
   cut -d : -f 2,3 |
   tr -d \" |
   wget -qi - -O githooked.prod
